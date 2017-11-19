eb85e113e6f6d56bafef4b3a5f666ea951ceac24
Hustlemore <33529259+hustlemore@users.noreply.github.com>
2017-11-19 09:32:43 -0600

    Add files via upload

---
+++ post.php
@@ -0,0 +1,16 @@
+<? 
+//ini_set('display_errors', 'On');
+//error_reporting(E_ALL);
+$name = @ trim ($_POST['name']);
+$email = @ trim ($_POST['email']);
+$text = @ trim ($_POST['text']);
+
+
+#Send data
+mail ("contact@hustlemo.re", //Enter your E-Mail
+      "To the coolest Guys and Girls at Hustlemore!", // Theme your E-Mail
+      " Name:$name \n Email:$email \n Message: $text", //E-Mail included
+      "Content-type:text/plain; charset=utf-8");
+
+header ("Location: http://wearesupermodels.com/Number_countdown/Video/thanks.html");
+?>
---
+++ subs.php
@@ -0,0 +1,21 @@
+<? 
+//ini_set('display_errors', 'On');
+//error_reporting(E_ALL);
+$email = @ trim ($_POST['email']);
+
+
+//mailchimp add subscrible
+include_once 'MailChimp.php';
+$MailChimp = new \Drewm\MailChimp('e1b129eb55bee6479c176f15e21e78c1-us17');
+$result = $MailChimp->call('lists/subscribe', array(
+                'id'                => 'cebbf19715',
+                'email'             => array('email' => $email),
+                'merge_vars'        => false,
+                'double_optin'      => true,
+                'update_existing'   => true,
+                'replace_interests' => false,
+                'send_welcome'      => false,
+            ));
+//print_r($result);
+header ("Location: http://wearesupermodels.com/Number_countdown/Video/thanks_sub.html");
+?>
