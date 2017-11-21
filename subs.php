b4c7e631520507d2b072eacbba37510d2a06b2a0
Hustlemore <33529259+hustlemore@users.noreply.github.com>
2017-11-21 16:31:57 -0600

    Update subs.php

--- subs.php
+++ subs.php
@@ -17,5 +17,5 @@ $result = $MailChimp->call('lists/subscribe', array(
                 'send_welcome'      => false,
             ));
 //print_r($result);
-header ("Location: https://hustlemore.github.io/dev/thanks_sub.html");
+header ("Location: thanks_sub.html");
 ?>
